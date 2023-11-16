e44e7fb1380e9139e54dbb0ccdf450bee6d2a2fa
yankyhermawan <yankyhermawan42@gmail.com>
2023-11-16 21:50:15 +0700

    Remove AutoMigrate on main

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"alfred/database"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	db := database.InitDB()
-	database.MigrateDB(db)
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, "Hello, world!")
