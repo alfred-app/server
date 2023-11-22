34dbe649bd1500f8d0f293ad728d0a6c1a7ece0c
yankyhermawan <yankyhermawan42@gmail.com>
2023-11-22 21:49:42 +0700

    Fix: CORS Error
    - Add middleware for the CORS

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"alfred/middleware"
 	"alfred/talent"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
+	}))
 	clientGroup := router.Group("/client")
 	talentGroup := router.Group("/talent")
 
@@ -20,7 +25,7 @@ func main() {
 
 	clientGroup.POST("/register", client.RegisterHandler)
 	clientGroup.POST("/login", client.LoginHandler)
-	clientGroup.GET("/:clientID", middleware.AuthenticationMiddleware, client.GetClientData)
+	clientGroup.GET("/:clientID", middleware.AuthenticationMiddleware, middleware.AuthorizationMiddleware, client.GetClientData)
 	clientGroup.PATCH("/:clientID", client.UpdateHandler)
 	clientGroup.PATCH("/change-password/:clientID", client.ChangePasswordHandler)
 
